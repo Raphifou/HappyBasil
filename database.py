6a2635df5687d8679c380f2bb42052555bdcae24
Raphifou <raphael.neri83@gmail.com>
2021-03-27 23:32:39 +0100

    Create database.py

---
+++ database.py
@@ -0,0 +1,25 @@
+import os
+import mariadb
+
+#Set up the database
+def	clear_db(light_state, motor_state, mode_state, pi_state):
+  try:
+    statement = "INSERT INTO employees (first_name,last_name) VALUES (%s, %s)"
+    data = (first_name, last_name)
+    cursor.execute(statement, data)
+    connection.commit()
+    print("Successfully added entry to database")
+    except database.Error as e:
+      print(f"Error adding entry to database: {e}")
+
+def read_db(light_state, motor_state,mode_state,pi_state):
+
+def update_db(temp, light, humidity, moisture, curr_time, curr_date):
+    try:
+        statement = "INSERT INTO employees (first_name,last_name) VALUES (%s, %s)"
+        data = (first_name, last_name)
+        cursor.execute(statement, data)
+        connection.commit()
+        print("Successfully added entry to database")
+    except database.Error as e:
+        print(f"Error adding entry to database: {e}")
